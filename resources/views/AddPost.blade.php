f87abd8cc116737882ede61043e24b98aaae5a0b
Marcel Alefierowicz <mrclalefierowicz@gmail.com>
2022-01-15 00:29:56 +0100

    updated dashboard and the add-post view

--- resources/views/AddPost.blade.php
+++ resources/views/AddPost.blade.php
@@ -1,45 +1,45 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Add a post to the forum') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('Add a post to the forum') }}
+      </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto">
-            <div class=" bg-slate-50 overflow-hidden shadow-xl sm:rounded-lg">
-                <div class= ''>
-                    <form class="w-full max-w-lg" method="POST" action='{{ route('insert') }}'>
-                      @csrf
-                        <div class=" -mx-3 mb-6">
-                          <div class="w-full px-3 mb-6">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                              Title
-                            </label>
-                            <input name='title' class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" />
-                          </div>
-                          <div class="w-full px-3 mb-6">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                             Content
-                            </label>
-                            <textarea name='content' cols='15' rows="5" class="appearance-none block 
-                                             w-full bg-gray-200 text-gray-700
-                                            border border-gray-200 rounded py-3 px-4
-                                             leading-tight focus:outline-none focus:bg-white
-                                              focus:border-gray-500" id="grid-last-name"></textarea>
-                            
-                          </div>
-                          <div class="w-full px-3 mb-6">
-                            <button type='submit' class='shadow-lg rounded-md bg-indigo-300 text-white px-4 py-4'
-                            >
-                            submit
-                            </button>
-                          </div>
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto">
+          <div class=" bg-slate-50 overflow-hidden shadow-xl sm:rounded-lg">
+              <div class= 'w-full'>
+                  <form class="w-full px-4 pt-4 max-w-lg" method="POST" action='{{ route('insert') }}'>
+                    @csrf
+                      <div class="p-4 -mx-3 mb-6">
+                        <div class="w-full px-3 mb-6">
+                          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                            Title
+                          </label>
+                          <input name='title' class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" />
                         </div>
-                    </form>
+                        <div class="w-full px-3 mb-6">
+                          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                           Content
+                          </label>
+                          <textarea name='content' cols='15' rows="5" class="appearance-none block 
+                                           w-full bg-gray-200 text-gray-700
+                                          border border-gray-200 rounded py-3 px-4
+                                           leading-tight focus:outline-none focus:bg-white
+                                            focus:border-gray-500" id="grid-last-name"></textarea>
+                          
+                        </div>
+                        <div class="w-full px-3 mb-6">
+                          <button type='submit' class='shadow-lg rounded-md bg-indigo-300 text-white px-4 py-4'
+                          >
+                          submit
+                          </button>
+                        </div>
+                      </div>
+                  </form>
 
-                </div>
-            </div>
-        </div>
-    </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </x-app-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,28 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Homepage') }}
-        </h2>
-    </x-slot>
+    <div class='bg-indigo-300'>
+        <x-slot name="header">
+            <h2 class="font-semibold  text-xl text-gray-800 leading-tight">
+                {{ __('Homepage') }}
+            </h2>
+        </x-slot>
+    </div>
     @foreach ($posts->reverse() as $post)
-        <div class="py-12">
+        <div class="px-12 py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class=" bg-slate-50 overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class= 'flex items-center justify-center h-screen '>
-                        <h1 class='mx-5 py-5 text-xl'>
-                        {{$post->title}}
-                        
-                        @if(Auth::user()->administrator)
-                            <p>
-                                CHUj
-                            </p>
+                <div class="bg-slate-50 h-auto overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class= 'px-4 items-center justify-center'>
+                        <h1 class='mx-5 py-5 text-2xl rounded'>
+                            <a class='font-bold underline decoration-rose-400'>{{$post->title}}</a> <span class=' text-xs ml-3 uppercase font-bold'>by {{$post->username}}</span>
+
+                            <span class='text-xs uppercase font-bold'>  on {{$post->created_at}} </span>
+                        @if($post->username == Auth::user()->name or Auth::user()->administrator == 1)
+                            <a class='ml-6 text-xs text-red-900 uppercase font-bold'> delete </a>
                         @endif
                         </h1>
 
+                        <p class='mx-5 py-5 text-lg'>
+                            {{$post->content}}
+                        </p>
                     </div>
                 </div>
             </div>
